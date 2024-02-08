3f9b1b4e6067fcb531b26bbd0ee523f5e63953a0
himberski <157299754+himberski@users.noreply.github.com>
2024-02-08 21:15:47 +0100

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function()
 {
 	// The formsubmit.co URL where the data is being sent to
-	const formSubmit = "https://formsubmit.co/maciej_malinowski@icloud.com";
+	const formSubmit = "https://formsubmit.co/maciej_malinowski@yahoo.de";
 
 	// The form element
 	let form = document.forms.namedItem('kontaktform');
