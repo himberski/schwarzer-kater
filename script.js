d33374e87880987300aa6cda55696e1a28fcf75f
himberski <157299754+himberski@users.noreply.github.com>
2024-02-08 22:21:16 +0100

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', function()
 {
-	// The hash-like string to send the emails to, from formsubmit.co
-	const key = 'f685c4dd961cf75a0db2d581839e42bc';
 	// The formsubmit.co URL where the data is being sent to
-	const formSubmit = 'https://formsubmit.co/maciej_malinowski@yahoo.de' + key;
+	const formSubmit = 'https://formsubmit.co/maciej_malinowski@yahoo.de';
 
 	// The form element
 	let form = document.forms.namedItem('kontaktform');
